e4c349193f795086544ddce5bc5f783c2713ce61
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-09-24 22:21:09 +0700

    update

--- NetworkPrograming/exercise2/grading.c
+++ NetworkPrograming/exercise2/grading.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_STUDENTS 100
 
@@ -10,8 +11,37 @@ typedef struct {
     char lastName[20];
     double progressMark;
     double finalExamMark;
+    double mark;
     char grade;
 } Student;
+
+void removeSpaces(char *str) {
+    if (str == NULL) {
+        return; // Handle NULL input
+    }
+
+    int len = strlen(str);
+    if (len == 0) {
+        return; // Empty string, nothing to remove
+    }
+
+    // Remove trailing spaces
+    while (len > 0 && (isspace(str[len - 1]) || str[len - 1] == '\t')) {
+        len--;
+    }
+    str[len] = '\0'; // Null-terminate the trimmed string
+
+    // Remove leading spaces
+    int start = 0;
+    while (isspace(str[start]) || str[start] == '\t') {
+        start++;
+    }
+
+    // Shift the string to remove leading spaces
+    if (start > 0) {
+        memmove(str, str + start, len - start + 1); // +1 for the null terminator
+    }
+}
 
 void calculateGradingSummary(const char *filename) {
     FILE *file = fopen(filename, "r");
@@ -19,6 +49,7 @@ void calculateGradingSummary(const char *filename) {
         printf("Error: Unable to open file %s\n", filename);
         return;
     }
+
 
     char subjectID[20];
     char subjectName[50];
@@ -39,6 +70,18 @@ void calculateGradingSummary(const char *filename) {
     fscanf(file, "Semester|%s\n", semesterID);
     fscanf(file, "StudentCount|%d\n", &studentCount);
 
+    char outputFileName[50];
+    strcat(outputFileName, subjectID);
+    strcat(outputFileName, "_");
+    strcat(outputFileName, subjectID);
+    strcat(outputFileName, "_rp.txt");
+    FILE *outputFile = fopen(outputFileName, "a+");
+    if (outputFile == NULL) {
+        printf("Error: Unable to open output file %s\n", outputFileName);
+        fclose(file);
+        return;
+    }
+
     // Read student information
     for (int i = 0; i < studentCount; i++) {
         fscanf(file, "S|%[^|]|%[^|]|%[^|]|%[^|]|%[^|]| %c |\n",
@@ -49,44 +92,43 @@ void calculateGradingSummary(const char *filename) {
                finalString,
                &students[i].grade);
 
-                double temp = 0;
-               for (int j=0; j<6; j++){
-                printf("M %c\n",progessString[j]);
-                if (progessString[j] == '.'){
-                    temp = (progessString[j-2]==49?10.0:0.0) + (0.0 + progessString[j-1] - 48) + (0.0 + (progessString[j+1] - 48))/10 + (0.0 + (progessString[j+2]>48?progessString[j+2]:48 - 48))/100;
-                    printf(" tot %lf\n",temp);
-                    break;
-                }
-               }
+        removeSpaces(students[i].firstName);
+        removeSpaces(students[i].lastName);
 
-               printf("std cnt %s \n", students[i].studentID);
-               printf("std cnt %s \n", students[i].firstName);
-               printf("std cnt %s \n", students[i].lastName);
-               printf("std cnt |%d| \n", students[i].progressMark);
-               printf("std cnt |%s| \n", finalString);
-               printf("std cnt %c \n", students[i].grade);
+        for (int j = 0; j < 6; j++) {
+            if (progessString[j] == '.') {
+                students[i].progressMark = (progessString[j - 2] == '1' ? 10.0 : 0.0) +
+                                           (0.0 + progessString[j - 1] - '0') +
+                                           (0.0 + (progessString[j + 1] - '0')) / 10 +
+                                           (0.0 + (progessString[j + 2] > '0' ? progessString[j + 2] - '0' : 0)) / 100;
+            }
+            if (finalString[j] == '.') {
+                students[i].finalExamMark = (finalString[j - 2] == '1' ? 10.0 : 0.0) +
+                                             (0.0 + finalString[j - 1] - '0') +
+                                             (0.0 + (finalString[j + 1] - '0')) / 10 +
+                                             (0.0 + (finalString[j + 2] > '0' ? finalString[j + 2] - '0' : 0)) / 100;
+            }
+        }
 
-        averageMark += (students[i].progressMark * progressWeight +
-                        students[i].finalExamMark * finalExamWeight);
-        if (students[i].grade > students[highestMarkIndex].grade)
+        students[i].mark += (students[i].progressMark * progressWeight + students[i].finalExamMark * finalExamWeight) / 100;
+
+        averageMark += students[i].mark;
+        if (students[i].mark > students[highestMarkIndex].mark)
             highestMarkIndex = i;
-        if (students[i].grade < students[lowestMarkIndex].grade)
+        if (students[i].mark < students[lowestMarkIndex].mark)
             lowestMarkIndex = i;
     }
     averageMark /= studentCount;
 
-    fclose(file);
-
-    // Output grading summary
-    printf("The student with the highest mark is: %s\n %s\n %s\n",
-           students[highestMarkIndex].firstName,
-           students[highestMarkIndex].lastName,
-           students[highestMarkIndex].studentID);
-    printf("The student with the lowest mark is: %s %s %s\n",
-           students[lowestMarkIndex].firstName,
-           students[lowestMarkIndex].lastName,
-           students[lowestMarkIndex].studentID);
-    printf("The average mark is: %.2lf\n", averageMark);
+    // Output grading summary to the output file
+    fprintf(outputFile, "The student with the highest mark is: %s %s\n",
+            students[highestMarkIndex].firstName,
+            students[highestMarkIndex].lastName);
+    fprintf(outputFile, "The student with the lowest mark is: %s %s\n",
+            students[lowestMarkIndex].firstName,
+            students[lowestMarkIndex].lastName);
+    fprintf(outputFile, "The average mark is: %.2lf\n", averageMark);
+    fprintf(outputFile, "\nA histogram of the subject %s is: \n", subjectID);
 
     // Calculate histogram
     int gradeCounts[5] = {0};
@@ -110,28 +152,32 @@ void calculateGradingSummary(const char *filename) {
         }
     }
 
-    // Output histogram
-    printf("A:");
+    // Output histogram to the output file
+    fprintf(outputFile, "A:");
     for (int i = 0; i < gradeCounts[0]; i++) {
-        printf("*");
+        fprintf(outputFile, "*");
     }
-    printf("\nB:");
+    fprintf(outputFile, "\nB:");
     for (int i = 0; i < gradeCounts[1]; i++) {
-        printf("*");
+        fprintf(outputFile, "*");
     }
-    printf("\nC:");
+    fprintf(outputFile, "\nC:");
     for (int i = 0; i < gradeCounts[2]; i++) {
-        printf("*");
+        fprintf(outputFile, "*");
     }
-    printf("\nD:");
+    fprintf(outputFile, "\nD:");
     for (int i = 0; i < gradeCounts[3]; i++) {
-        printf("*");
+        fprintf(outputFile, "*");
     }
-    printf("\nF:");
+    fprintf(outputFile, "\nF:");
     for (int i = 0; i < gradeCounts[4]; i++) {
-        printf("*");
+        fprintf(outputFile, "*");
     }
-    printf("\n");
+    fprintf(outputFile, "\n");
+
+    // Close both input and output files
+    fclose(file);
+    fclose(outputFile);
 }
 
 int main() {
