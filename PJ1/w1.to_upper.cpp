69e92390739a95d0b079ff7f500c526e90e0bb4a
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-02 15:22:37 +0700

    uppercase

---
+++ PJ1/w1.to_upper.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char upper_case_of(char c){
+	char C = c;
+	if (c<=122&&c>=97) //(c.isalpha() && c.islower()) 
+	{
+		C -= 32;
+	}
+	return C;
+}
+
+int main(){
+	char c;
+	while(cin >> c){
+		cout << upper_case_of(c);
+	}
+}
+// a - 97
+// A - 65
+// z - 122
+// Z - 90
