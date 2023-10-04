16d170f49c5e56363925cfa0d233fd3933adfe67
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-04 09:40:09 +0700

    Update w1.degree_2.cpp

--- PJ1/w1.degree_2.cpp
+++ PJ1/w1.degree_2.cpp
@@ -3,14 +3,12 @@ using namespace std;
 
 void degree_2(int a, int b, int c){
     float delta = (float)(b*b - 4*a*c);
-    if(delta<0) printf("NO SOLUTION");
+    if(delta<0) cout << "NO SOLUTION";
     else if(delta == 0){
-        printf("%.2f" , (float) -b/(2*a));
+        printf("%.2f", (float)(-b/(2*a)));
     }
     else{
-        float x2 = (float)(-b+sqrt(delta))/(2*a);
-        float x1 = (float)(-b-sqrt(delta))/(2*a);
-        printf("%.2f %.2f" , x1, x2);
+        printf("%.2f %.2f", (float)(-b+sqrt(delta))/(2*a), (float)(-b-sqrt(delta))/(2*a));
     }
 }
 
