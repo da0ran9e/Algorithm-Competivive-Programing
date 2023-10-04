683d12fdf44947e2edcbb415df935f7910568754
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-04 21:34:12 +0700

    Create w1.text_replacement.cpp

---
+++ PJ1/w1.text_replacement.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string s1 , s2 , t;
+	getline(cin,s2);
+	getline(cin,s1);
+	getline(cin, t);
+	
+	string buf;
+	size_t pos=0;
+	size_t prePos;
+
+	buf.reserve(t.size());
+
+	while(true) {
+		prePos = pos;
+		pos = t.find(s2, pos);
+		if (pos == string::npos){
+			break;
+		}
+		buf.append(t, prePos, pos - prePos);
+		buf+=s1;
+		pos+=s2.size();
+
+	}
+buf.append(t, prePos, t.size() - prePos);
+t.swap(buf);
+
+
+cout << t << endl;	
+}
