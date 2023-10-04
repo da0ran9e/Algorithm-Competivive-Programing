eaf17d2b90a30a87f535c024b3816f96791e7dc6
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-04 10:41:38 +0700

    Update w1.EVN.cpp

--- PJ1/w1.EVN.cpp
+++ PJ1/w1.EVN.cpp
@@ -1,38 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define VAT(a) a+a/10;
-
-float odd_bill(int amount){
+float old_bill(int amount){
 	if(amount>=0 && amount<=50)
-		return (float)amount*1.728;
+		return (float)amount*1728;
 	else if(amount>=51 && amount <=100)
-		return (float)amount*1.786;
+		return 50*1728+(float)(amount-50)*1786;
 	else if(amount>=101 && amount<=200)
-		return (float)amount*2.074;
+		return 50*1728+50*1786+(float)(amount-100)*2074;
 	else if(amount>=201 && amount <=300)
-		return (float)amount*2.612;
+		return 50*1728+50*1786+100*2074+(float)(amount-200)*2612;
 	else if(amount>=301 && amount<=400)
-		return (float)amount*2.919;
+		return 50*1728+50*1786+100*2074+100*2612+(float)(amount-300)*2919;
 	else	
-		return (float)amount*3.015;
+		return 50*1728+50*1786+100*2074+100*2612+100*2919+(float)(amount-400)*3015;
+}
+
+float vat(float bill){
+	return bill + bill/10;
 }
 
 float new_bill(int amount){
 	if(amount>=0 && amount<=100)
-		return (float)amount*1.728;
+		return (float)amount*1728;
 	else if(amount>=101 && amount <=200)
-		return (float)amount*2.074;
+		return 100*1728+(float)(amount-100)*2074;
 	else if(amount>=201 && amount <=400)
-		return (float)amount*2.612;
+		return 100*1728+100*2074+(float)(amount-200)*2612;
 	else if(amount>=401 && amount<=700)
-		return (float)amount*3.111;
+		return 100*1728+100*2074+200*2612+(float)(amount-400)*3111;
 	else	
-		return (float)amount*3.457;
+		return 100*1728+100*2074+200*2612+300*3111+(float)(amount-700)*3457;
 }
 
 int main(){
 	int amount;
 	cin >> amount;
-	cout << VAT(odd_bill(amount)) - VAT(new_bill(amount));
+	cout << -vat(old_bill(amount))+vat(new_bill(amount));
 }
