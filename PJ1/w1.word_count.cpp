0d98111e804a9ae6db6dea63427a2ea9968a89da
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-04 21:38:48 +0700

    Update w1.word_count.cpp

--- PJ1/w1.word_count.cpp
+++ PJ1/w1.word_count.cpp
@@ -6,7 +6,7 @@ int main(){
 	char temp;
 	int cnt = 0;
 	while(cin.get(c)){
-		if((c==' ' || c=='\n') && ((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z') || (temp >= '0' && temp <= '9'))) cnt++;
+		if((c==' ' || c=='\n' || c=='-' || c==',' || c=='_' || c=='.') && ((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z') || (temp >= '0' && temp <= '9'))) cnt++;
 		temp = c;
 	}
 	if((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z') || (temp >= '0' && temp <= '9')) cnt++;
