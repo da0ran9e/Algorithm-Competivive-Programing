0af69ee79efefdb2ed2eafeef9f6e16551169b5f
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-05 15:00:34 +0700

    Update w2.sudoku_count.cpp

---
+++ PJ1/w2.sudoku_count.cpp
@@ -0,0 +1,89 @@
+#include <bít/stdc++.h>
+using namespace std;
+
+const int N = 9;
+
+// check if num is valid for the given row
+bool is_row_valid(int board[N][N], int row, int num) {
+	for (int col = 0; col < N; col++) {
+		if (board[row][col] == num) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// check if num is valid for the given column
+bool is_col_valid(int board[N][N], int col, int num) {
+	for (int row = 0; row < N; row++) {
+		if (board[row][col] == num) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// check if num is valid for the 3x3 grid that (row, col) belongs to
+bool is_grid_valid(int board[N][N], int row, int col, int num) {
+	int start_row = row - row % 3;
+	int start_col = col - col % 3;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (board[start_row + i][start_col + j] == num) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+// check if it's safe to place num at board[row][col]
+bool is_safe(int board[N][N], int row, int col, int num) {
+	return is_row_valid(board, row, num) && 
+		   is_col_valid(board, col, num) && 
+		   is_grid_valid(board, row, col, num);
+}
+
+// recursive function to find the number of solutions
+int sudoku_solver(int board[N][N], int row, int col) {
+	// base case: if all cells have been filled, return 1 (solution found)
+	if (row == N) {
+		return 1;
+	}
+	
+	// if the current column is the last column, move to the next row
+	if (col == N) {
+		return sudoku_solver(board, row + 1, 0);
+	}
+	
+	// if the current cell is not empty, move to the next cell
+	if (board[row][col] != 0) {
+		return sudoku_solver(board, row, col + 1);
+	}
+	
+	// try out all possible numbers for the current cell
+	int count = 0;
+	for (int num = 1; num <= N; num++) {
+		if (is_safe(board, row, col, num)) {
+			board[row][col] = num;
+			count += sudoku_solver(board, row, col + 1);
+			board[row][col] = 0;
+		}
+	}
+	return count;
+}
+
+int main() {
+	int input_board[N][N];
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> input_board[i][j];
+		}
+	}
+
+		
+	cout << sudoku_solver(input_board, 0, 0) << "\n";
+
+	return 0;
+}
