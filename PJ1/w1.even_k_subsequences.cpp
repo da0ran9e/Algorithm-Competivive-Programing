6388627ab829d4fc9ea16395e27e328b56187a0f
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-05 07:24:41 +0700

    Update w1.even_k_subsequences.cpp

--- PJ1/w1.even_k_subsequences.cpp
+++ PJ1/w1.even_k_subsequences.cpp
@@ -3,28 +3,19 @@ using namespace std;
 
 #define MAX_N 100000
 
-int subarrays(int n, int k, int *x){
-	int count = 0;
-	int sum[MAX_N] = {0};
-
-	for (int i=1; i<n; i++){
-		sum[i] = sum[i-1] + x[i]; // 
-	}
-
-	for (int i=k-1; i<n; i++){
-		int seq_sum = sum[i] - (i>=k ? sum[i-k]:0);
-		if(seq_sum%2 == 0){
-			count ++;
-		}
-	}
-	return count;
-}
-
 int main(){
 	int n, k, x[MAX_N];
+	int count=0;
 	cin >> n >> k;
 	for (int i = 0; i<n; i++){
 		cin >> x[i];
+		if(i>=k){
+			int sum=0;
+			for(int j=k; j>0; j--) {
+				sum+=x[i-j];
+			}
+			if(sum%2==0) count++; 
+		}
 	}
-	cout << subarrays(n,k,x);
+	cout << count;
 }
