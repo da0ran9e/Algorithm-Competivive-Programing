85041b05d6dbb3449b207ef34d3ae43f18c3d5c4
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-06 10:42:34 +0700

    Update w2.permutation.cpp

--- PJ1/w2.permutation.cpp
+++ PJ1/w2.permutation.cpp
@@ -8,21 +8,17 @@ bool existed[NMAX];
 
 void Try(int k, int n) {
     for (int i = 1; i <= n; i++) {
-        if (!existed[i]) { // If integer i hasn't been existed yet
-            x[k] = i;     // Choose integer i
-            existed[i] = true; // Mark it as existed
+        if (!existed[i]) { // check if i has been existed or not
+            x[k] = i;
+            existed[i] = true;
 
             if (k == n) {
-                // Print the permutation
                 for (int j = 1; j <= n; j++)
                     cout << x[j] << " ";
                 cout << endl;
             } else {
-                // Recursively generate the next element in the permutation
                 Try(k + 1, n);
             }
-
-            // Backtrack: unchoose integer i and mark it as not existed
             existed[i] = false;
         }
     }
