d3ebcf4d6069c531e9265b798136c5a999d787e4
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-06 11:33:38 +0700

    Update w2.C_K_N.cpp

--- PJ1/w2.C_K_N.cpp
+++ PJ1/w2.C_K_N.cpp
@@ -1,20 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define min(a,b) a<b?a:b
 #define MAX 100
 
 int M[MAX][MAX];// M[k][n] store the value of C(k,n)
 
 int C(int k,int n){
-    if(k == 0 || k == n) M[k][n] = 1;
-    else if(M[k][n] == 0)
-      M[k][n] = C(k,n-1) + C(k-1,n-1);
-    return M[k][n];
+    k = min(k, n-k); // eg: 3C5 = 2C5
+    int a[k], b[k]; 
+    int D;
+    long long result = 1;
+
+    for (int i = 0; i < k; i++) {
+        a[i] = n-k+1+i; // n! / (n-k)! = n(n-1)...(n-k+1)
+        b[i] = i+1; // k!
+    }
+
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            if (b[i] == 1) break;
+            D = std::__gcd(b[i], a[j]); //greatest common divisor 
+            a[j] = a[j]/D;
+            b[i] = b[i]/D;
+        }
+    }
+
+    for (int i=0; i<k; i++){
+        result *= a[i];
+    }
+
+    return result;
 }
 
 int main(){
     int a,b;
     cin >> a >> b;
-    memset(M,0,sizeof(M));
     cout << C(a, b);
 }
