ae149422d5b59c5b7149351736214069ec28fa13
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-07 07:57:37 +0700

    Update w2.C_K_N.cpp

--- PJ1/w2.C_K_N.cpp
+++ PJ1/w2.C_K_N.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 #define min(a,b) a<b?a:b
 #define MAX 100
+#define MOD 1000000007
 
 int M[MAX][MAX];// M[k][n] store the value of C(k,n)
 
@@ -10,7 +11,7 @@ int C(int k,int n){
     k = min(k, n-k); // eg: 3C5 = 2C5
     int a[k], b[k]; 
     int D;
-    long long result = 1;
+    long long int result = 1;
 
     for (int i = 0; i < k; i++) {
         a[i] = n-k+1+i; // n! / (n-k)! = n(n-1)...(n-k+1)
@@ -27,7 +28,7 @@ int C(int k,int n){
     }
 
     for (int i=0; i<k; i++){
-        result *= a[i];
+        result = (result*a[i])%MOD;
     }
 
     return result;
