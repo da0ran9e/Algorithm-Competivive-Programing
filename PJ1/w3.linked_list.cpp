c297ce4d341d42c3f886b8d24746d0ed1588334b
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-09 23:14:40 +0700

    Update w3.linked_list.cpp

--- PJ1/w3.linked_list.cpp
+++ PJ1/w3.linked_list.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
     int n;
+    string command;
     vector<int> linked_list;
     cin >> n;
 
@@ -15,22 +16,40 @@ int main(){
     while (cin>>command && command!="#")
     {
         if (command == "addlast"){
-            cin>>value; 
+            int v;
+            cin>>v; 
+            linked_list.push_back(v);
         }
         else if (command == "addfirst"){
-
+            int v;
+            cin>>v; 
+            linked_list.insert(linked_list.begin(), v);
         }
         else if (command == "addafter"){
-
+            int v, p;
+            cin>>v>>p; 
+            
+            auto it = find(linked_list.begin(), linked_list.end(), p);
+            linked_list.insert(it + 1, v);
         }
         else if (command == "addbefore"){
-
+            int v, p;
+            cin>>v>>p; 
+            
+            auto it = find(linked_list.begin(), linked_list.end(), p);
+            linked_list.insert(it, v);
         }
         else if (command == "remove"){
+            int v;
+            cin>>v;
 
+            linked_list.erase(remove(linked_list.begin(), linked_list.end(), v), linked_list.end());
         }
         else if (command == "reverse"){
-
+            reverse(linked_list.begin(), linked_list.end());
         }
+    }
+    for (int i : linked_list) {
+        cout << i << " ";
     }
 }
