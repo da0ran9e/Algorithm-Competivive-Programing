3718a210456d391c10e22d84f0eb6d8d1f4db845
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-15 08:27:43 +0700

    Update w4.store_and_search.cpp

--- PJ1/w4.store_and_search.cpp
+++ PJ1/w4.store_and_search.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
     set<string> words;
-    string word;
+    string word, command;
+
     while (cin >> word && word != "*") {
         words.insert(word);
     }
-    string command;
+
     while (cin >> command) {
         if (command == "***") {
             break;
         }
 
         cin >> word;
+
         if (command == "find") {
-            cout << (words.count(word) > 0 ? 1 : 0) << endl;
+            cout << words.count(word) > 0 ? 1 : 0 << endl;
         } else if (command == "insert") {
             pair<set<string>::iterator, bool> result = words.insert(word);
             cout << (result.second ? 1 : 0) << endl;
