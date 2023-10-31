f21aa902658a82caa8b8cfd2fdfe96ba7cd1a7ff
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-31 14:54:12 +0700

    Create w6.max_flow.cpp

---
+++ PJ1/w6.max_flow.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+const int INF = 1e9;
+
+struct Edge {
+    int to, capacity, flow;
+};
+
+std::vector<std::vector<Edge>> graph;
+std::vector<int> parent;
+
+int maxFlow(int source, int sink) {
+    int flow = 0;
+    while (true) {
+        std::fill(parent.begin(), parent.end(), -1);
+        std::queue<std::pair<int, int>> q;
+        q.push({source, INF});
+
+        while (!q.empty()) {
+            int cur = q.front().first;
+            int capacity = q.front().second;
+            q.pop();
+
+            for (Edge& edge : graph[cur]) {
+                int next = edge.to;
+                if (parent[next] == -1 && edge.capacity - edge.flow > 0) {
+                    int new_capacity = std::min(capacity, edge.capacity - edge.flow);
+                    parent[next] = cur;
+                    if (next == sink) {
+                        flow += new_capacity;
+                        int v = next;
+                        while (v != source) {
+                            int u = parent[v];
+                            for (Edge& e : graph[u]) {
+                                if (e.to == v) {
+                                    e.flow += new_capacity;
+                                    break;
+                                }
+                            }
+                            for (Edge& e : graph[v]) {
+                                if (e.to == u) {
+                                    e.flow -= new_capacity;
+                                    break;
+                                }
+                            }
+                            v = u;
+                        }
+                        break;
+                    }
+                    q.push({next, new_capacity});
+                }
+            }
+        }
+        if (parent[sink] == -1) break;
+    }
+    return flow;
+}
+
+int main() {
+    int N, M;
+    std::cin >> N >> M;
+    int source, sink;
+    std::cin >> source >> sink;
+
+    graph.resize(N);
+    parent.resize(N);
+
+    for (int i = 0; i < M; i++) {
+        int u, v, c;
+        std::cin >> u >> v >> c;
+        graph[u - 1].push_back({v - 1, c, 0});
+        graph[v - 1].push_back({u - 1, 0, 0});
+    }
+
+    int max_flow = maxFlow(source - 1, sink - 1);
+    std::cout << max_flow << std::endl;
+
+    return 0;
+}
