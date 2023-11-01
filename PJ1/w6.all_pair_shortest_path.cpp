23fbe1ec12af8cab1c21e03086e1f1901f532dc9
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-11-01 09:28:16 +0700

    Update w6.all_pair_shortest_path.cpp

--- PJ1/w6.all_pair_shortest_path.cpp
+++ PJ1/w6.all_pair_shortest_path.cpp
@@ -1,48 +1,45 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
 
 const int INF = 1e9;
 
 int main() {
     int n, m;
-    std::cin >> n >> m;
+    cin >> n >> m;
     
-    // Initialize the distance matrix with infinity
-    std::vector<std::vector<int>> dist(n, std::vector<int>(n, INF));
-    
-    // Set the diagonal elements to 0
-    for (int i = 0; i < n; ++i) {
+  
+    vector<vector<int>> dist(n, vector<int>(n, INF));
+    for (int i=0; i<n; ++i) {
         dist[i][i] = 0;
     }
     
-    // Read the edges and weights
-    for (int i = 0; i < m; ++i) {
+    for (int i=0; i<m; ++i) {
         int u, v, w;
-        std::cin >> u >> v >> w;
-        dist[u - 1][v - 1] = w; // Adjust for 0-based indexing
+        cin >> u >> v >> w;
+        dist[u - 1][v - 1] = w; 
     }
     
-    // Floyd-Warshall algorithm
-    for (int k = 0; k < n; ++k) {
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
+    // Floyd-Warshall
+    for (int k=0; k<n; ++k) {
+        for (int i=0;i<n; ++i) {
+            for (int j=0; j<n; ++j) {
                 if (dist[i][k] != INF && dist[k][j] != INF) {
-                    dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
     }
     
-    // Print the distance matrix
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
+    // Print
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
             if (dist[i][j] == INF) {
-                std::cout << -1 << " ";
+                cout << -1 << " ";
             } else {
-                std::cout << dist[i][j] << " ";
+                cout << dist[i][j] << " ";
             }
         }
-        std::cout << std::endl;
+        cout << endl;
     }
     
     return 0;
